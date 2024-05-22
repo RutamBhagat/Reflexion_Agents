4798d29f1ac7f4f6cf9bc18310e5c3ad142d9d77
RutamBhagat <rutambhagat@gmail.com>
2024-05-22 17:52:38 +0530

    add revisor chain

--- app/schemas.py
+++ app/schemas.py
@@ -14,3 +14,9 @@ class AnswerQuestion(BaseModel):
     reflection: Reflection = Field(description="Your reflection on the initial answer")
     search_queries: List[str] = Field(description="""1-3 search queries for researching improvements
                                       to address the critique of your current answer.""")
+    
+
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question"""
+
+    references: List[str] = Field(description="Citations motivating your updated answer")    
